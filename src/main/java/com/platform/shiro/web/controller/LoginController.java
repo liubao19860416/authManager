fee145ae1c46a725f80fe6a8b04018ecf3a316f1
liubao <18611478781@163.com>
2016-10-12 08:25:39 +0800

    添加读取文件方式

--- src/main/java/com/platform/shiro/web/controller/LoginController.java
+++ src/main/java/com/platform/shiro/web/controller/LoginController.java
@@ -1,8 +1,10 @@
 package com.platform.shiro.web.controller;
 
-import java.io.FileInputStream;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -33,12 +35,15 @@ public class LoginController {
     	return "pdf";
     }
     
-    @RequestMapping(value = "/test/getPdfStream")
+	@RequestMapping(value = "/test/getPdfStream")
     public void getPdfStream(HttpServletRequest request,HttpServletResponse response, Model model){
-    	String file="D:\\temp\\test.pdf";
+    	//String filepath="D:\\temp\\test.pdf";
+    	String filepath = "https://pdfobject.com/pdf/sample-3pp.pdf";
 		InputStream in = null;
 		try {
-			in = new FileInputStream(file);
+			//in = new FileInputStream(filepath);
+			in=getInputStream(filepath);
+			
 			ServletOutputStream out = response.getOutputStream();
 			StreamUtils.copy(in, out);
 		} catch ( IOException e) {
@@ -55,7 +60,83 @@ public class LoginController {
     	
     }
     
-    @RequestMapping(value = "/mall/login")
+	private InputStream getInputStream(String filepath) {
+		// 创建url;
+		URL url = null;
+		HttpURLConnection urlconn = null;
+		BufferedInputStream bis = null;
+		try {
+			url = new URL(filepath);
+			// 创建url连接;
+			urlconn = (HttpURLConnection) url.openConnection();
+			// 链接远程服务器;
+			urlconn.connect();
+			// 获取远程服务器端文件输入流;
+			bis = new BufferedInputStream(urlconn.getInputStream());
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		return bis;
+	}
+    
+    @SuppressWarnings("unused")
+	private InputStream getInputStream2() {
+    	String filepath = "https://pdfobject.com/pdf/sample-3pp.pdf";
+
+    	 //创建url;
+    	 URL url=null;
+    	 HttpURLConnection urlconn =null;
+		try {
+			url = new URL(filepath);
+			//创建url连接;
+			urlconn = (HttpURLConnection)url.openConnection();
+			//链接远程服务器;
+			urlconn.connect();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+    	BufferedInputStream  bis = null;
+//    	ServletOutputStream os =null;
+    	//BufferedOutputStream   bos   =   null;
+    	 try{
+    	   //获取远程服务器端文件输入流;
+    	   bis = new BufferedInputStream(urlconn.getInputStream() );
+
+    	   /*os = response.getOutputStream();
+    	    bos   =  new   BufferedOutputStream(response.getOutputStream());   
+    	   //缓存;  
+    	    byte b[]=new byte[2048];    
+    	   //客户使用保存文件的对话框：   
+    	   response.setHeader("Content-disposition","inline;filename="+filename);
+    	   //通知客户文件的MIME类型：   
+    	   response.setContentType("application/octet-stream exe;charset=utf-8");   
+    	   
+    	   int size;
+    	   //读取文件内容到缓存;
+    	   while((size=bis.read(b,0,b.length))!=-1)   
+    	   {   //把文件内容写到本地文件中;
+    	   //bos.write(b,0,size);   
+    	    os.write(b,0,size);
+    	   }*/
+    	  }catch (Exception e) {
+    	   e.printStackTrace();
+    	  }/*finally{
+    	  
+    	   if(fos != null)
+    	   { //关闭文件输出流;
+    	    fos.close();
+    	   }
+    	   if(bis != null)
+    	   { //关闭文件输入流;
+    	    bis.close();
+    	    urlconn.disconnect();
+    	   }
+    	  }*/
+		return bis;
+	}
+
+	@RequestMapping(value = "/mall/login")
     public String showMallLoginForm(HttpServletRequest req, Model model){
     	return doLogin(req,model ,"mall/login");
     }
